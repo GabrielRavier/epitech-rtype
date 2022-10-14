07f4707855ca82197e2cac7e68ef625a20f6f304
Alphabet <alphabet@lumzapp.com>
2022-10-14 21:38:26 +0200

    feat(client): Add network support base.

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -21,14 +21,48 @@
 #include <ctime>
 #include <chrono>
 #include <thread>
+#include <boost/asio.hpp>
+#include <boost/array.hpp>
+#include "../server/sync_queue.hpp"
+#include "../server/packets/packet.hpp"
 
+boost::asio::ip::udp::socket *_socket;
+SynchronisedQueue<Packet> queue;
 Coordinator gCoordinator;
 
-int main(int argc, char *argv[])
+void NetworkLoop(const char *host, uint16_t port)
 {
-    loguru::init(argc, argv);
-    loguru::add_file("r-type_client.log", loguru::Append, loguru::Verbosity_MAX);
-    LOG_F(INFO, "Starting client");
+    try {
+        boost::asio::io_service io_service;
+        boost::asio::ip::udp::resolver resolver(io_service);
+        boost::asio::ip::udp::resolver::query query(boost::asio::ip::udp::v4(), host, "");
+        boost::asio::ip::udp::endpoint target_endpoint = *resolver.resolve(query);
+        boost::asio::ip::udp::endpoint sender_endpoint;
+
+        _socket = new boost::asio::ip::udp::socket(io_service);
+        _socket->open(boost::asio::ip::udp::v4());
+
+        // Set port of connection.
+        target_endpoint.port(port);
+
+        boost::array<char, 1> send_buf = { 10 };
+        _socket->send_to(boost::asio::buffer(send_buf), target_endpoint);
+
+        while (_socket->is_open()) {
+            char *packet = new char[199];
+            _socket->receive_from(boost::asio::buffer(packet, 199), sender_endpoint);
+            std::string address = sender_endpoint.address().to_string() + std::to_string(sender_endpoint.port());
+            std::cout << "TEST." << std::endl << address << std::endl;
+        }
+
+    } catch (std::exception &e) {
+        std::cerr << e.what() << std::endl;
+    }
+}
+
+void GameLoop(const char *host, uint16_t port)
+{
+    std::thread threadNetworkLoop(NetworkLoop, host, port);
 
     const std::shared_ptr<WindowManager> windowManager = std::make_shared<WindowManager>();
     windowManager->Init("R-Type", 1920, 700);
@@ -128,8 +162,29 @@ int main(int argc, char *argv[])
         std::this_thread::sleep_for(std::chrono::milliseconds(delta_ms_duration.count()));
     }
 
+    // Close socket and wait network thread stop.
+    _socket->close();
+    threadNetworkLoop.join();
+
     // We call gCoordinator.Clear() before exiting the program, so as to make sure that destructors for the game objects
     // run before the SFML is destroyed at the global level
     gCoordinator.Clear();
+}
+
+int main(int argc, char *argv[])
+{
+    loguru::init(argc, argv);
+    loguru::add_file("r-type_client.log", loguru::Append, loguru::Verbosity_MAX);
+    LOG_F(INFO, "Starting client");
+
+    // Check arguments.
+    if (argc < 3) {
+        std::cout << "Try: " << argv[0] << " [host] [port]." << std::endl;
+        return (0);
+    }
+
+    // Start network and game.
+    GameLoop(argv[1], std::atoi(argv[2]));
+
     return (0);
 }
--- src/server/main.cpp
+++ src/server/main.cpp
@@ -15,15 +15,22 @@ std::map<std::string, NetworkManager> managers;
 void NetworkLoop(uint16_t port)
 {
     try {
+        uint8_t buf[4096];
+        boost::asio::mutable_buffer buffer = boost::asio::buffer(buf, 4096);
         boost::asio::io_context io_context;
         ip::udp::socket         socket(io_context, ip::udp::endpoint(ip::udp::v4(), port));
         ip::udp::endpoint       remote_endpoint;
 
         while (true) {
-            char *packet = new char[199];
-            socket.receive_from(boost::asio::buffer(packet, 199), remote_endpoint);
+            socket.receive_from(buffer, remote_endpoint);
             std::string address = remote_endpoint.address().to_string() + std::to_string(remote_endpoint.port());
-            std::cout << "TEST." << std::endl << address << std::endl;
+
+            if (managers.find(address) == managers.end()) {
+                managers[address] = NetworkManager();
+
+            } else {
+
+            }
         }
 
     } catch (std::exception &e) {
@@ -40,8 +47,6 @@ void GameLoop()
     while (running) {
         start = std::chrono::system_clock::now();
 
-        std::cout << "TEST." << std::endl;
-
         const std::chrono::duration<double, std::milli> work_time = std::chrono::system_clock::now() - start;
         const std::chrono::duration<double, std::milli> delta_ms((1000 / fps) - work_time.count());
         auto delta_ms_duration = std::chrono::duration_cast<std::chrono::milliseconds>(delta_ms);
@@ -51,6 +56,11 @@ void GameLoop()
 
 int main(int argc, char **argv)
 {
+    if (argc < 2) {
+        std::cout << "Try: " << argv[0] << " [port]." << std::endl;
+        return (0);
+    }
+
     std::thread threadNetworkLoop(NetworkLoop, std::atoi(argv[1]));
     std::thread threadGameLoop(GameLoop);
 
