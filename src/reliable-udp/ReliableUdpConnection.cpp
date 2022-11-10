28d3ddbc9ac6e4b655286e04fda2c630c1d5f6d4
Gabriel Ravier <gabravier@gmail.com>
2022-11-10 17:15:35 +0100

    fix(network): Forgot to make both arguments to process_blocks pointers
    (instead of iterators, which are not convertible to void *)

--- src/reliable-udp/ReliableUdpConnection.cpp
+++ src/reliable-udp/ReliableUdpConnection.cpp
@@ -240,7 +240,7 @@ void ReliableUdpConnection::receiveThreadFunction()
                     ReliableUdpConnection::our_crc64_type crc64_checksum;
                     crc64_checksum.process_block(
                         &packetHeaderBytes.at(ReliableUdpConnection::packetBeginMagic.size() + sizeof(std::uint64_t)),
-                        packetHeaderBytes.end());
+                        packetHeaderBytes.data() + packetHeaderBytes.size());
                     for (std::size_t i = ReliableUdpConnection::packetHeaderSize;
                          i < ReliableUdpConnection::packetHeaderSize + receivedDataSize; ++i)
                         crc64_checksum.process_byte(individualConnection->second.bufferedBytes.at(i));
